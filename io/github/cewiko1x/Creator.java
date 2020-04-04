cee9326e9444a8f8e7b81728060bb4c2c5683b2e
cewiko1x <62911010+cewiko1x@users.noreply.github.com>
2020-04-04 16:15:02 +0200

    Update B1.0.3

--- io/github/cewiko1x/Creator.java
+++ io/github/cewiko1x/Creator.java
@@ -1,6 +1,8 @@
 package io.github.cewiko1x;
 
 
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_COLOR_BURNPeer;
+import jdk.nashorn.internal.scripts.JO;
 import org.apache.commons.io.FileUtils;
 
 import javax.swing.*;
@@ -19,10 +21,10 @@ public class Creator implements ActionListener {
     public static URL artifactsURL;
     public static URL cfxURL;
     File fiveMServer = null;
+    File artifactsFile = null;
     String appData = System.getenv("APPDATA");
     String cache = appData + "/SC";
     File cacheFile = new File(cache);
-    File artifactFile;
     File cfxFile;
     File serverCfg;
     File starter;
@@ -37,20 +39,75 @@ public class Creator implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JButton installButton = new JButton("Start Install");
         JButton directoryButton = new JButton("Select FiveM Server Directory");
+        JButton artifactsButton = new JButton("Select Artifacts");
         JButton githubButton = new JButton("Github");
+        JButton clearCacheButton = new JButton("Clear Cache");
+        JButton artifactsURLButton = new JButton("Artifacts Download");
         JLabel title = new JLabel("FXServer Creator");
         title.setFont(new Font("Verdana", Font.PLAIN, 18));
         title.setVisible(true);
         title.setBounds(10,0,300,40);
+        installButton.setVisible(true);
         installButton.setBounds(10,50,200,30);
         installButton.setFont(new Font("Verdana", Font.PLAIN, 14));
+        installButton.addActionListener(this);
+        artifactsButton.setVisible(true);
+        artifactsButton.setBounds(10,260,200,30);
+        artifactsButton.setFont(new Font("Verdana", Font.PLAIN, 14));
+        clearCacheButton.setVisible(true);
+        clearCacheButton.setBounds(10,200,200,30);
+        clearCacheButton.setFont(new Font("Verdana", Font.PLAIN, 14));
+        artifactsURLButton.setVisible(true);
+        artifactsURLButton.setBounds(10,300,200,30);
+        artifactsURLButton.setFont(new Font("Verdana", Font.PLAIN, 14));
         githubButton.setVisible(true);
         githubButton.setBounds(10, 160, 200, 30);
 
-        installButton.addActionListener(this);
-
         directoryButton.setBounds(10,100, 200, 50);
         directoryButton.setFont(new Font("Verdana", Font.PLAIN, 10));
+
+        clearCacheButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(cacheFile != null) {
+                    try {
+                        FileUtils.deleteDirectory(cacheFile);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+
+            }
+        });
+
+        artifactsURLButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI("https://runtime.fivem.net/artifacts/fivem/build_server_windows/master/"));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (URISyntaxException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        artifactsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setCurrentDirectory(new java.io.File("."));
+                chooser.setDialogTitle("Select Downloaded Artifacts");
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    artifactsFile = chooser.getSelectedFile();
+                    System.out.println("Artifacts Directory is: " + artifactsFile.getAbsolutePath());
+                }
+                else {
+                    System.out.println("No Selection ");
+                }
+            }
+        });
 
         githubButton.addActionListener(new ActionListener() {
             @Override
@@ -92,7 +149,10 @@ public class Creator implements ActionListener {
 
         frame.add(installButton);
         frame.add(githubButton);
+        frame.add(artifactsButton);
         frame.add(directoryButton);
+        frame.add(artifactsURLButton);
+        frame.add(clearCacheButton);
         frame.add(title);
         frame.setSize(400,400);
         frame.setLayout(null);
@@ -113,10 +173,12 @@ public class Creator implements ActionListener {
             return;
         }
 
+        if(artifactsFile == null) {
+            return;
+        }
+
         try {
-            artifactFile = new File(cacheFile.getAbsolutePath() + "/artifacts.zip");
             cfxFile = new File(cacheFile.getAbsolutePath() + "/cfx.zip");
-            FileUtils.copyURLToFile(artifactsURL, artifactFile);
             FileUtils.copyURLToFile(cfxURL, cfxFile);
         } catch (IOException e) {
             e.printStackTrace();
@@ -124,7 +186,7 @@ public class Creator implements ActionListener {
 
         try {
           zipUtility.unzip(cfxFile.getAbsolutePath(), fiveMServer.getAbsolutePath());
-          zipUtility.unzip(artifactFile.getAbsolutePath(), fiveMServer.getAbsolutePath());
+          zipUtility.unzip(artifactsFile.getAbsolutePath(), fiveMServer.getAbsolutePath());
             System.out.println("Unzipped CFX And Artifacts");
             serverCfg = new File(fiveMServer.getAbsolutePath() + "/cfx-server-data-master/server.cfg");
             starter = new File(fiveMServer.getAbsolutePath() + "/starter.bat");
@@ -148,8 +210,7 @@ public class Creator implements ActionListener {
             String license = "";
 
             if(serverCfg != null) {
-               license = String.valueOf(
-                        JOptionPane.showInputDialog(this, "License key (keymaster.fivem.net:"));
+               license = String.valueOf(JOptionPane.showInputDialog("Enter your server license key (keymaster.fivem.net)"));
             }
 
             ZipUtility.modifyFile(serverCfg.getAbsolutePath(), "changeme", license);
